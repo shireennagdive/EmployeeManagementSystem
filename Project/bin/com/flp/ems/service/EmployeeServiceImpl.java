1c4dc11343def3aa6292223825bc9a3e49638709
shiru4 <shireen.nagdive54@gmail.com>
2016-10-06 14:58:04 +0530

    Add files via upload

---
+++ Project/bin/com/flp/ems/service/EmployeeServiceImpl.java
@@ -0,0 +1,71 @@
+package com.flp.ems.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import com.flp.ems.dao.EmployeeDaoImplForDB;
+import com.flp.ems.dao.EmployeeDaoImplForList;
+import com.flp.ems.domain.Employee;
+
+public class EmployeeServiceImpl implements IEmployeeService{
+
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	Date date=null;
+	EmployeeDaoImplForList dao = new EmployeeDaoImplForList();
+	EmployeeDaoImplForDB dbdao = new EmployeeDaoImplForDB();
+	public void AddEmployee(HashMap<String, String> hm){
+		Employee emp = new Employee();
+		emp.setName(hm.get("Name"));
+		emp.setKin_Id(emp.getName()+Employee.getCount());
+		emp.setAdress(hm.get("Adress"));
+		emp.setEmail_id(emp.getName()+Employee.getCount()+"@barclays.com");
+		emp.setPhone_no(Long.parseLong(hm.get("PhoneNumber")));
+		emp.setDepartmentId(Integer.parseInt(hm.get("DepartmentID")));
+		emp.setProjectID(Integer.parseInt(hm.get("ProjectID")));
+		emp.setRoleID(Integer.parseInt(hm.get("RoleID")));
+		try {
+			date=dateFormat.parse(hm.get("DateOfBirth"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		emp.setDateofbirth(date);
+		try {
+			date=dateFormat.parse(hm.get("DateOfJoining"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		emp.setDateofjoin(date);
+		dbdao.AddEmployee(emp);
+	}
+	public void ModifyEmployee(HashMap<String, String> hm){
+		Employee e = dbdao.search(hm.get("KinID"),hm.get("Name"),hm.get("EmployeeID"));
+		e.setAdress(hm.get("Adress"));
+		e.setPhone_no(Long.parseLong(hm.get("PhoneNumber")));
+		dbdao.ModifyEmployee(e);
+		/*List<Employee> list = dao.getList();
+		Iterator<Employee> it = list.iterator();
+		for(Employee e : list){
+			System.out.println("id is : "+hm.get("KinID"));
+			if(hm.get("KinID").equals(e.getKin_Id())){
+				System.out.println("in equals");
+				
+			}
+		}*/
+	}
+	public void RemoveEmployee(String temp){
+		dbdao.RemoveEmployee(temp);
+	}
+	public void SearchEmployee(String temp){
+		dbdao.SearchEmployee(temp); //comparator
+	}
+	public void getAllEmployee(){
+		dbdao.getAllEmployee();
+	}
+	 
+}
