a63c81d74393e55437fab9586a7afda2af250290
shiru4 <shireen.nagdive54@gmail.com>
2016-10-07 11:02:24 +0530

    Add files via upload

---
+++ DynamicWeb2/src/com/web/SessionServlet.java
@@ -0,0 +1,50 @@
+package com.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/SessionServlet")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//HttpSession ses = request.getSession();
+		//System.out.println(ses.getId());
+		Cookie cookie = new Cookie("Sample","Sample");
+		response.addCookie(cookie);
+		//System.out.println(ses.getId());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String encodedURL=response.encodeURL("getDate");
+		response.sendRedirect(encodedURL);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
